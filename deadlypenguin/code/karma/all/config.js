48ee2bc423a884a441872492b7804e37af844f18
Patrick Connelly <patrick@deadlypenguin.com>
2018-04-03 14:19:15 -0400

    Updating for cloudant move

--- deadlypenguin/code/karma/all/config.js
+++ deadlypenguin/code/karma/all/config.js
@@ -6,7 +6,7 @@ var config = {};
 config.DATE_FORMAT = 'YYYY-MM-DD';
 config.MIN_DATE = '2012-10-26';
 
-config.BASE_URL = 'https://pcon.cloudant.com/karma';
+config.BASE_URL = 'https://da56fe52-708f-4acb-8890-af3178f0960d-bluemix.cloudant.com/karma';
 config.URL_SINGLE_NICK_TEMPLATE = config.BASE_URL + '/_design/karma/_view/single_nick/?startkey=[{{{nick}}}{{#if startdate}},"{{startdate}}"{{/if}}]&endkey=[{{{nick}}},{{#if enddate}}"{{enddate}}"{{else}}{}{{/if}}]';
 config.URL_MULTI_NICK_TEMPLATE = config.BASE_URL + '/_design/karma/_view/by_nick/?keys=[{{{nicks}}}]';
 config.SEARCH_TEMPLATE = '?nick={{nick}}{{#if to}}&to={{to}}{{/if}}{{#if from}}&from={{from}}{{/if}}';
